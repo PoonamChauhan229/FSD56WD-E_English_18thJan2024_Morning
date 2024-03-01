716569bf5db7751b7a461a2864db04de76062802
Poonam229 <poonam.chauhan229@gmail.com>
2024-03-01 12:50:55 +0530

    crud app using mockapi

--- 20day/script.js
+++ 20day/script.js
@@ -3,6 +3,7 @@ let email=document.getElementById('email')
 let url = `https://652e7b520b8d8ddac0b1761f.mockapi.io/users`;
 let itemlist=document.getElementById('itemlist')
 
+//Part1 : POSTING THE DATA TO THE SERVER
 function submitBtn(){
     console.log(1)
     console.log(username.value,email.value)
@@ -10,6 +11,8 @@ function submitBtn(){
 }
 
 async function createUser(value1,value2) {
+  console.log(value1,value2)
+  if(value1 !='' && value2 !=''){
     let newUser = {
       name: value1,
       email: value2,
@@ -24,9 +27,17 @@ async function createUser(value1,value2) {
       const {id}=data
 
     getUserSpecificData(id);
+    // BUG2
+    username.value='';
+    email.value='';
+
+  }else{
+    // BUG1
+    alertbootstrap("Enter the Username and Email","danger")
+  }
 }
 
-//   Part2 : FETCHING THE DATA AND DISPLAY IN TABLE
+//Part2 : FETCHING THE DATA FROM THE SERVER AND DISPLAYING IN THE TABLE
 
 // table
 function createTableRow(value1,value2,id){
@@ -41,8 +52,11 @@ function createTableRow(value1,value2,id){
 
     let td3=document.createElement('td')
     td3.innerHTML=`
-                    <button class="btn btn-danger" type="button"><i class="bi bi-archive-fill"></i></button>
-                    <button class="btn btn-warning" type="button"><i class="bi bi-pencil-square"></i></button>
+                    <button type="button" class="btn btn-primary" id='edit${id}' onclick="editUser('${id}','${value1}','${value2}')"data-bs-toggle="modal" data-bs-target='#exampleModal${id}'>
+                    <i class="bi bi-pencil-square"></i>
+                    </button>
+
+                    <button class="btn btn-danger" id='delete${id}' onclick='deleteUser(${id})' type="button"><i class="bi bi-archive-fill"></i></button>
                 `;  
 
     // <th scope="row">1</th>
@@ -76,3 +90,96 @@ async function getUserSpecificData(userId) {
         const {name,email,id}=data
         createTableRow(name,email,id)
 }
+
+//Part 3 : DELETE THE DATA  
+
+async function deleteUser(deleteUserId) {
+  console.log(deleteUserId)
+ alert("Are You Sure?")
+  let res = await fetch(`${url}/${deleteUserId}`, {
+    method: "DELETE",
+  });
+  itemlist.innerHTML="";
+  getUserData()
+}
+
+
+//Part 4: EDIT BUTTON > DESIGN THE FORM > SHOULD HAVE THE SPECIFIC DATA 
+const editUser=(id,value1,value2)=>{
+  console.log(id,value1)
+  const modal=document.createElement('span')
+  modal.innerHTML=`
+                    <div class="modal fade" id="exampleModal${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                      <div class="modal-content">
+                        <div class="modal-header">
+                          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+
+                        <div class="modal-body">
+                          <div class="input-group mb-3">
+                          <span class="input-group-text" id="basic-addon1">UserName</span>
+                          <input type="text" id="modalusername${id}" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" value="${value1}">
+                        </div>
+            
+                      <div class="input-group mb-3">
+                        <span class="input-group-text" id="basic-addon1">Email</span>
+                        <input id='modalemail${id}' type="email" class="form-control" placeholder="Email" aria-label="Username" aria-describedby="basic-addon1" value="${value2}">
+                      </div>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                          <button type="button" class="btn btn-primary" onclick='updateUser(${id})'>Save changes</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>  
+                  `
+// append
+document.body.append(modal)
+}
+
+
+//Part 4: UPDATE THE DATA
+
+async function updateUser(updateUserId) {
+      console.log(updateUserId)
+    
+      let updatedUserName=document.getElementById('modalusername'+updateUserId).value
+      let updatedEmail=document.getElementById('modalemail'+updateUserId).value
+      console.log(updatedUserName,updatedEmail)
+        try {
+          let updateDetail = {
+            name: updatedUserName,
+            email: updatedEmail
+          };
+          let res = await fetch(`${url}/${updateUserId}`, {
+            method: "PUT",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify(updateDetail),
+          });
+
+          let data = await res.json();
+          console.log(data);
+          const {name,email}=data
+          document.getElementById('name'+updateUserId).innerText=name
+          document.getElementById('email'+updateUserId).innerText=email
+
+        } catch (e) {
+          console.log(e);
+        }
+       }
+
+//PART 5: ALERT
+var alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+var alertTrigger = document.getElementById('liveAlertBtn')
+
+function alertbootstrap(message, type) {
+  var wrapper = document.createElement('div')
+  wrapper.innerHTML = '<div class="alert alert-' + type + ' alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+  alertPlaceholder.append(wrapper)
+}
+       
+       
